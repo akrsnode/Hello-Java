5f33a963ddb21986b90f9a9be9d27cc8ccdc1977
vansmoe <vansmoe@hotmail.com>
2020-12-07 14:54:23 +0100

    isPalindome handle null

---
+++ src/tst/classes/ex/NewNewMain.java
@@ -0,0 +1,13 @@
+package tst.classes.ex;
+
+public class NewNewMain {
+
+    private static boolean isPalindrome(String text) {
+        if (text == null) return false;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(null));
+    }
+}
