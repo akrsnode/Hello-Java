79f7d2d3c0124e9e947d5c40c0ebc8bb361d2892
vansmoe <vansmoe@hotmail.com>
2020-12-07 13:52:21 +0100

    handle null array

--- src/tst/classes/ex/NewMain.java
+++ src/tst/classes/ex/NewMain.java
@@ -2,4 +2,20 @@ package tst.classes.ex;
 
 public class NewMain {
 
+    private static int findSecondGreatest(int[] array) {
+        if (array == null) {
+            throw new IllegalArgumentException("Array is null.");
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+//        int array[] = { 1, 2, 3, 4, 5 };
+        int[] array = null;
+
+        System.out.println(findSecondGreatest(array));
+
+
+    }
 }
