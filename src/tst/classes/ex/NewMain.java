20696833493cc0e3c686f0fcb30ca3f2cda15c4a
vansmoe <vansmoe@hotmail.com>
2020-12-07 14:02:39 +0100

    handle too short array

--- src/tst/classes/ex/NewMain.java
+++ src/tst/classes/ex/NewMain.java
@@ -4,18 +4,20 @@ public class NewMain {
 
     private static int findSecondGreatest(int[] array) {
         if (array == null) {
-            throw new IllegalArgumentException("Array is null.");
+            throw new IllegalArgumentException("Array is null");
+        }
+        if (array.length < 2) {
+            throw new IllegalArgumentException("Array contain not enough elements: " + array.length);
         }
         return 0;
     }
 
     public static void main(String[] args) {
 
-//        int array[] = { 1, 2, 3, 4, 5 };
         int[] array = null;
 
+//        System.out.println(findSecondGreatest(array));
+        array = new int[]{1};
         System.out.println(findSecondGreatest(array));
-
-
     }
 }
