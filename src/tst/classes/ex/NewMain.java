759bb81860d97912463edafcb4750b93142fdb29
vansmoe <vansmoe@hotmail.com>
2020-12-07 14:20:30 +0100

    Method findSecondGreatest finished

--- src/tst/classes/ex/NewMain.java
+++ src/tst/classes/ex/NewMain.java
@@ -1,5 +1,7 @@
 package tst.classes.ex;
 
+import java.util.Arrays;
+
 public class NewMain {
 
     private static int findSecondGreatest(int[] array) {
@@ -9,15 +11,24 @@ public class NewMain {
         if (array.length < 2) {
             throw new IllegalArgumentException("Array contain not enough elements: " + array.length);
         }
-        return 0;
+        Arrays.sort(array);
+        return array[array.length-2];
     }
 
     public static void main(String[] args) {
-
         int[] array = null;
-
 //        System.out.println(findSecondGreatest(array));
         array = new int[]{1};
+//        System.out.println(findSecondGreatest(array));
+        array = new int[]{1, 2};
+        System.out.println(findSecondGreatest(array));
+        array = new int[]{1, 2, 3, 4, 5};
+        System.out.println(findSecondGreatest(array));
+        array = new int[]{2, 1};
+        System.out.println(findSecondGreatest(array));
+        array = new int[]{-5, 61, -31, 0};
+        System.out.println(findSecondGreatest(array));
+        array = new int[]{-5, -61, -31, 0};
         System.out.println(findSecondGreatest(array));
     }
 }
